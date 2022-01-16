ce081588b29d3fb55f83a33a407ee81238243807
makefu <github@syntax-fehler.de>
2022-01-16 20:59:06 +0100

    speech_recognition: fetch cmusphinx-data via fetchurl

--- speech_recognition.nix
+++ speech_recognition.nix
@@ -6,8 +6,14 @@
 , flac
 , callPackage
 , python
+, fetchurl
 }:
-
+let
+  cmusphinx-data = fetchurl {
+    url = "https://deac-riga.dl.sourceforge.net/project/cmusphinx/Acoustic%20and%20Language%20Models/German/cmusphinx-de-voxforge-5.2.tar.gz";
+      sha256 = "0gci1k26ziysqmn5jjhsr6dlwrn9yhdnwnamibvx15x69nbyvkg9";
+    };
+in
 buildPythonPackage rec {
   pname = "speech_recognition";
   version = "3.8.1";
@@ -19,7 +25,7 @@ buildPythonPackage rec {
   postInstall = ''
     datadir=$out/lib/${python.libPrefix}/site-packages/speech_recognition/pocketsphinx-data/de-DE/
     mkdir -p "$datadir"
-    tar xf ${./cmusphinx-de-voxforge-5.2.tar.gz} -C /tmp
+    tar xf ${cmusphinx-data} -C /tmp
     indir=/tmp/cmusphinx-cont-voxforge-de-r20171217
     mv $indir/etc/voxforge.lm.bin $datadir/language-model.lm.bin
     mv $indir/etc/voxforge.dic $datadir/pronounciation-dictionary.dict
