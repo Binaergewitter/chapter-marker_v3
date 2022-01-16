17c5211a3775ef73ef018d42c5e007d948a42124
makefu <github@syntax-fehler.de>
2022-01-16 20:58:27 +0100

    chunks2text: use stdout, write log to stderr

--- chunks2text.py
+++ chunks2text.py
@@ -10,9 +10,13 @@ from pathlib import PurePath
 import os
 from docopt import docopt
 import json
+import logging
+
+logging.basicConfig(level=logging.INFO)
+log = logging.getLogger("chunks2text")
 
 def guess_sendungsnummer():
-    print("Retrieving current bgt show number")
+    log.info("Retrieving current bgt show number")
     url = "https://pad.binaergewitter.de/"
     ret = urllib.request.urlopen(url)
     return ret.geturl().split("/")[-1]
@@ -24,7 +28,7 @@ def main():
     r = sr.Recognizer()
 
     start = time.time()
-    print(f"Start tts at {start} with dir {folder_name} ({len(os.listdir(folder_name))} files)")
+    log.info(f"Start tts at {start} with dir {folder_name} ({len(os.listdir(folder_name))} files)")
     ret = {}
     total_length = 0
     for f in sorted(os.listdir(folder_name)):
@@ -46,26 +50,22 @@ def main():
             #r.adjust_for_ambient_noise(source)
             audio_listened = r.record(source)
 
-            print(f"Handling file {f}")
+            logging.info(f"Handling file {f}")
             try:
                 text = r.recognize_google(audio_listened,language='de-DE')
             except sr.UnknownValueError as e:
-                print("Error:", str(e))
+                log.error(f"Error: {e}")
                 text = ""
             else:
                 text = f"{text.capitalize()}. "
-                print(" -- :", text)
-                # try converting it to text
+                logging.info(f"google: {text}")
             ret[f]['text']['google'] = text
-            print(ret[f])
+            log.debug(ret[f])
             time.sleep(3)
     end = time.time()
     time_needed = end-start
-    print(f"Finished tts at {end}, took {time_needed/60} Minutes")
-    with open(f"{folder_name.split('_')[0]}.json","w+") as f:
-        json.dump(ret,f)
-
-
+    log.info(f"Finished tts at {end}, took {time_needed/60} Minutes")
+    print(json.dumps(ret))
 
 
 if __name__ == "__main__":
